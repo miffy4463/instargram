cb773954cd48940bad8f668a6737358fb14badcb
김정민 <kjm4463@naver.com>
2023-03-23 16:40:26 +0900

    소스파일 커밋

---
+++ src/main/java/com/cos/instargram/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.cos.instargram.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    BCryptPasswordEncoder encode() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/auth/signin")
+                .defaultSuccessUrl("/");
+        return http.build();
+    }
+}
