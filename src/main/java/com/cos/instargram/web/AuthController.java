75ecb2a5342b4ac1ffa51c4b79093944c8cd49f7
김정민 <kjm4463@naver.com>
2023-03-24 15:17:44 +0900

    유효성 검사 추가, Global ControllerAdvice 추가

--- src/main/java/com/cos/instargram/web/AuthController.java
+++ src/main/java/com/cos/instargram/web/AuthController.java
@@ -1,13 +1,20 @@
 package com.cos.instargram.web;
 
 import com.cos.instargram.domain.user.User;
+import com.cos.instargram.handler.ex.CustomValidationException;
 import com.cos.instargram.service.AuthService;
 import com.cos.instargram.web.dto.auth.SignupReqDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor // final 필드를 모든 생성자를 만들어준다(DI)
@@ -27,11 +34,18 @@ public class AuthController {
     }
 
     @PostMapping("/auth/signup")
-    public String signup(SignupReqDto signupReqDto) { // key = value (x-www-form-urlencoded)
-        User user = signupReqDto.toEntity(); // SignupDto -> User
-        User userEntity = authService.join(user);
-        log.info(userEntity.toString());
-
-        return "/auth/signin";
+    public String signup(@Valid SignupReqDto signupReqDto, BindingResult bindingResult) { // key = value (x-www-form-urlencoded)
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            throw new CustomValidationException("유효성 검사 실패", errorMap);
+        } else {
+            User user = signupReqDto.toEntity(); // SignupDto -> User
+            User userEntity = authService.join(user);
+            log.info(userEntity.toString());
+            return "/auth/signin";
+        }
     }
 }
--- src/main/java/com/cos/instargram/web/dto/auth/SignupReqDto.java
+++ src/main/java/com/cos/instargram/web/dto/auth/SignupReqDto.java
@@ -3,11 +3,19 @@ package com.cos.instargram.web.dto.auth;
 import com.cos.instargram.domain.user.User;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 public class SignupReqDto {
+    @Size(min = 2, max = 20)
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
+    @NotBlank
     private String email;
+    @NotBlank
     private String name;
 
     public User toEntity() {
